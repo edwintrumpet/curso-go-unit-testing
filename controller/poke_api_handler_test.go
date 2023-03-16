3e3589113687c9989edec96e678c6c6d061e1f6b
Edwin Garcia <spark.com.co@gmail.com>
2023-03-15 23:12:27 -0500

    Llamando apis externas

---
+++ controller/poke_api_handler_test.go
@@ -0,0 +1,27 @@
+package controller
+
+import (
+	"catching-pokemons/models"
+	"encoding/json"
+	"io/ioutil"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetPokemonFromPokeApiSuccess(t *testing.T) {
+	c := require.New(t)
+
+	pokemon, err := GetPokemonFromPokeAPI("pikachu")
+	c.NoError(err)
+
+	body, err := ioutil.ReadFile("samples/pokeapi_response.json")
+	c.NoError(err)
+
+	var expected models.PokeApiPokemonResponse
+
+	err = json.Unmarshal([]byte(body), &expected)
+	c.NoError(err)
+
+	c.Equal(expected, pokemon)
+}
