ca7ff49fd0875f2f380bcc13ae2c2c19a0270bce
Edwin Garcia <spark.com.co@gmail.com>
2023-03-15 22:41:57 -0500

    manejo de errores y panic en go

--- util/parser_test.go
+++ util/parser_test.go
@@ -9,7 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestParserPokemonSuccess(t *testing.T) {
+func TestParsePokemonSuccess(t *testing.T) {
 	c := require.New(t)
 
 	body, err := ioutil.ReadFile("samples/pokeapi_response.json")
@@ -32,4 +32,22 @@ func TestParserPokemonSuccess(t *testing.T) {
 	c.NoError(err)
 
 	c.Equal(expected, parsedPokemon)
+}
+
+func TestParsePokemonTypeNotFound(t *testing.T) {
+	c := require.New(t)
+
+	body, err := ioutil.ReadFile("samples/pokeapi_response.json")
+	c.NoError(err)
+
+	var response models.PokeApiPokemonResponse
+
+	err = json.Unmarshal([]byte(body), &response)
+	c.NoError(err)
+
+	response.PokemonType = []models.PokemonType{}
+
+	_, err = ParsePokemon(response)
+	c.NotNil(err)
+	c.EqualError(ErrNotFoundPokemonType, err.Error())
 }
